fe0619766787b2662104f95232ea4dc0f8808128
AkashPandian <akashpandian.pandian@finstein.ai>
2023-07-29 13:14:49 +0530

    latitude module have been add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppserviceService } from './appservice.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'AngularGeoFencing';
+  location: any;
+  locationJs: any;
+
+constructor(private appservice:AppserviceService) {
+  
+}
+ 
+
+
+
+ngOnInit() {
+
+    this.appservice.getLocation().subscribe((responce)=>{
+      console.log(responce);
+      this.location=responce;     
+
+
+    })
+    navigator.geolocation.getCurrentPosition((position)=>{
+
+      console.log(position);
+      
+      this.locationJs=position.coords;
+        
+    })
+
+
+
+  }
+
+
+
 }
